8831f3f8e49315c05c468018e15996123590ab27
sduwal <sduwal@uno.edu>
2022-01-30 12:34:30 -0600

    Fixed logger issue

--- game/src/store/middleware/logger.js
+++ game/src/store/middleware/logger.js
@@ -1,20 +1,19 @@
-import { nanoid } from "@reduxjs/toolkit";
 import supabase from "../../supabase";
 
 const logger = (store) => (next) => async (action) => {
-    // console.group(action.type === "timer/decrement");
+    console.log(action.type);
     if (action.type !== "timer/decrement") {
-        await supabase.from("logs").insert({
-            userId: store.getState().interaction.userId,
-            type: action.type,
-            action: action.payload
-        });
+        // await supabase.from("logs").insert({
+        //     userId: store.getState().interaction.userId,
+        //     type: action.type,
+        //     action: action.payload
+        // });
         // console.group(action.type);
         // console.info("dispatching", action);
-        const result = next(action);
-        // console.groupEnd();
-        return result;
     }
+    const result = next(action);
+    // console.groupEnd();
+    return result;
 };
 
 export default logger;
