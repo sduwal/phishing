282d4910bcc8cdd5608807b23e89ad836c203905
sduwal <sduwal@uno.edu>
2022-03-15 20:57:58 -0500

    section 2 intial draft

--- game/src/store/middleware/logger.js
+++ game/src/store/middleware/logger.js
@@ -17,22 +17,22 @@ const logger = (store) => (next) => async (action) => {
     ];
 
     try {
-        if (!ignore.includes(action.type)) {
-            await supabase.from("logs").insert({
-                userId: store.getState().status.username,
-                type: action.type,
-                action: action.payload,
-                date: new Date().toString()
-            });
-        }
+        // if (!ignore.includes(action.type)) {
+        //     await supabase.from("logs").insert({
+        //         userId: store.getState().status.username,
+        //         type: action.type,
+        //         action: action.payload,
+        //         date: new Date().toString()
+        //     });
+        // }
     } catch (err) {
-        try {
-            await supabase.from("errors").insert({
-                error: err.message
-            });
-        } catch (err) {
-            // console.log(err);
-        }
+        // try {
+        //     await supabase.from("errors").insert({
+        //         error: err.message
+        //     });
+        // } catch (err) {
+        //     // console.log(err);
+        // }
     } finally {
         const result = next(action);
         return result;
